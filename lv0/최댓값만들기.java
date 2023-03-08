cee5eb2d30d7e0d89ca08a6098d0a70ef958f76f
KEN ROE <shdnrnjs@gmail.com>
2023-03-08 17:58:35 +0900

    '1'

--- lv0/최댓값만들기.java
+++ lv0/최댓값만들기.java
@@ -5,31 +5,31 @@ public class 최댓값만들기 {
 	public static void main(String[] args) {
 		int[] numbers = {2,2,2,2};
        
-//        int max = 0;
-//        int secmax = -1;
-//        
-//        for(int i=0; i<numbers.length; i++) {
-//        	if(numbers[i] > max) {
-//        		max = numbers[i];
-//        	}
-//        }
-//        
-//        //secmax를 못찾았을 때, 그걸 알 수 있는 경우는 for문 끝났을 때 
-//        for(int i=0; i<numbers.length; i++) {
-//        	if(numbers[i] > secmax && numbers[i] < max ) { //최댓값이 같은 경우?
-//        		secmax = numbers[i];
-//        	}
-//       
-//        }
-//        if(secmax == -1) {
-//        	secmax = max;
-//        }
-//
-//        int answer = max * secmax;
-//        
-//        System.out.println(max);
-//        System.out.println(secmax);        
-//        System.out.println(answer);
+        int max = 0;
+        int secmax = 0;
+        
+        for(int i=0; i<numbers.length; i++) {
+        	if(numbers[i] > max) {
+        		max = numbers[i];
+        	}
+        }
+        
+        //secmax를 못찾았을 때, 그걸 알 수 있는 경우는 for문 끝났을 때 
+        for(int i=0; i<numbers.length; i++) {
+        	if(numbers[i] > secmax && numbers[i] < max ) { //최댓값이 같은 경우?
+        		secmax = numbers[i];
+        	}
+       
+        }
+        if(secmax == 0) {
+        	secmax = max;
+        }
+
+        int answer = max * secmax;
+        
+        System.out.println(max);
+        System.out.println(secmax);        
+        System.out.println(answer);
 
 
 	}
