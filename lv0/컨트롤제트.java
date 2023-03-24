d498d9aac78f683eaa7a6ae5d2b58c4c72e01417
KEN ROE <shdnrnjs@gmail.com>
2023-03-24 14:09:55 +0900

    79

--- lv0/컨트롤제트.java
+++ lv0/컨트롤제트.java
@@ -5,7 +5,7 @@ public class 컨트롤제트 {
 	public static void main(String[] args) {
 		String s = "10 Z 20 Z 1";
 		int answer = 0;
-		int sum = 0;
+		
 		
 		//먼저 자른다음에 공백 제거 해야함
 		String str[] = s.split(" ");
@@ -17,12 +17,12 @@ public class 컨트롤제트 {
 		
 		for(int i=0; i<str.length; i++) {
 			if(str[i].equals("Z")) {
-				sum = sum - (Integer.parseInt(str[i-1]));
+				answer = answer - (Integer.parseInt(str[i-1]));
 				continue;
 			}
-			sum += Integer.parseInt(str[i]);
+			answer += Integer.parseInt(str[i]);
 		}
-		System.out.println(sum);
+		System.out.println(answer);
 	}
 
 }
