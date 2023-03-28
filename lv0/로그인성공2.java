40b4611d3ba19e679144a2b10fe594a5eeb66eb2
KEN ROE <shdnrnjs@gmail.com>
2023-03-28 18:02:03 +0900

    85?

---
+++ lv0/로그인성공2.java
@@ -0,0 +1,11 @@
+package Programmers_Java.lv0;
+
+public class 로그인성공2 {
+    public static void main(String[] args) {
+        String[] id_pw = {"meosseugi", "1234"};
+        String[][] db = {{"rardss", "123"}, {"yyoom", "1234"}, {"meosseugi", "1234"}};
+
+
+
+    }
+}
