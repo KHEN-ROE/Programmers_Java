e7301c937751bc4ab44936506505ffa45130080b
KEN ROE <shdnrnjs@gmail.com>
2023-03-30 09:17:56 +0900

    dd

--- lv0/HashMapPR.java
+++ lv0/HashMapPR.java
@@ -8,7 +8,7 @@ public class HashMapPR {
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap<>();
         map.put("kim", 010123123);
-        map.put("lee", 010123222);
+        map.put("lee", 010123222); //
 
         System.out.println(map.keySet());
         System.out.println(map.entrySet());
