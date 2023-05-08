0b4d91b1034c05faeb89d9dd0493aaf56caa0525
KEN ROE <shdnrnjs@gmail.com>
2023-05-08 18:02:07 +0900

    37 푸는중

---
+++ lv1/비밀지도.java
@@ -0,0 +1,10 @@
+package Programmers_Java.lv1;
+
+public class 비밀지도 {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+        String[] answer = {};
+    }
+}
