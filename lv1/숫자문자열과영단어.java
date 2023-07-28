bc6fda69d19cf3be542c8ee647408a4607e27e21
KEN <shdnrnjs@gmail.com>
2023-07-28 19:51:23 +0900

    lv1 37 완료

---
+++ lv1/숫자문자열과영단어.java
@@ -0,0 +1,86 @@
+package Programmers_Java.lv1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class 숫자문자열과영단어 {
+
+    public static void main(String[] args) {
+        String s = "one4seveneight";
+        solution(s);
+    }
+
+    public static int solution(String s) {
+        int answer = 0;
+
+        // map을 이용하여 해결
+        Map<String, Integer> map = new HashMap<>();
+        map.put("zero", 0);
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
+        map.put("six", 6);
+        map.put("seven", 7);
+        map.put("eight", 8);
+        map.put("nine", 9);
+
+        String[] str = s.split("");
+        String temp = "";
+
+        // 숫자를 포함하는 지 확인하는 정규식
+        String pattern = "^[0-9]*$";
+
+        List<Integer> list = new ArrayList<>();
+
+        for (String s1 : str) {
+            temp += s1;
+            if (Pattern.matches(pattern, temp)) {
+                list.add(Integer.parseInt(temp));
+                temp = "";
+            }
+            if (map.containsKey(temp)) {
+                list.add(map.get(temp));
+                temp = "";
+            }
+        }
+
+        // Integer 리스트를 하나의 int 필드로 변환하는 과정
+        StringBuilder sb = new StringBuilder();
+
+        // StringBuilder의 append 메서드는 다양한 타입의 매개변수 처리 가능
+        // append에 Integer를 전달하면 Integer의 toString메서드가 호출되어 정수가 문자열로 변환된다.
+        for (Integer integer : list) {
+            sb.append(integer);
+        }
+
+        answer = Integer.parseInt(sb.toString());
+
+
+        return answer;
+
+
+// 모범답안
+//        String[][] mapArr = {{"0", "zero"},
+//                {"1", "one"},
+//                {"2", "two"},
+//                {"3", "three"},
+//                {"4", "four"},
+//                {"5", "five"},
+//                {"6", "six"},
+//                {"7", "seven"},
+//                {"8", "eight"},
+//                {"9", "nine"}};
+//
+//        for (String[] map : mapArr) {
+//            s = s.replace(map[1], map[0]);
+//        }
+//
+//        int answer = Integer.parseInt(s);
+//        return answer;
+    }
+}
--- lv1/핸드폰번호가리기.java
+++ lv1/핸드폰번호가리기.java
@@ -3,14 +3,14 @@ package Programmers_Java.lv1;
 public class 핸드폰번호가리기 {
     public static void main(String[] args) {
         String phone_number = "027778888";
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         String[]str = phone_number.split("");
 
         for(int i = 0; i < str.length-4; i++){
             str[i] = str[i].replaceAll(str[i], "*");
         }
-        for (int i = 0; i < str.length; i++) {
-            answer += str[i];
+        for (String s : str) {
+            answer.append(s);
         }
         System.out.println(answer);
     }
